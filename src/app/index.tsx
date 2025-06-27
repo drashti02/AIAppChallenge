1cd06630877300f2a53912321b186b5382d25d96
Suchi Shah <suchi.s@xcitech.in>
2025-06-27 21:25:09 +0530

    expo router added

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="justify-center flex-1 bg-white items-center dark:bg-black">
+      <Text className="text-3xl text-black font-bold">HomeScreen</Text>
+    </View>
+  );
+}
